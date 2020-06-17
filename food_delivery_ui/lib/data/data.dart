a0d1d3bc30da605e678f153286bc916eabcf7c3b
prashantchanne12 <prashantchanne121@gmail.com>
2020-06-17 21:26:22 +0530

    Screenshots updated and minor changes

--- food_delivery_ui/lib/data/data.dart
+++ food_delivery_ui/lib/data/data.dart
@@ -24,36 +24,36 @@ final _salmon = Food(
 // Restaurants
 final _restaurant0 = Restaurant(
   imageUrl: 'assets/images/restaurant0.jpg',
-  name: 'Restaurant 0',
-  address: '200 Main St, New York, NY',
+  name: 'Maratha',
+  address: 'Pratikshanagar, Sion, 400022',
   rating: 5,
   menu: [_burrito, _steak, _pasta, _ramen, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant1 = Restaurant(
   imageUrl: 'assets/images/restaurant1.jpg',
-  name: 'Restaurant 1',
-  address: '200 Main St, New York, NY',
+  name: 'Sunshine',
+  address: 'Sion, Mumbai, 400022',
   rating: 4,
   menu: [_steak, _pasta, _ramen, _pancakes, _burger, _pizza],
 );
 final _restaurant2 = Restaurant(
   imageUrl: 'assets/images/restaurant2.jpg',
-  name: 'Restaurant 2',
-  address: '200 Main St, New York, NY',
+  name: 'Night Villa',
+  address: 'Bandra, Mumbai',
   rating: 4,
   menu: [_steak, _pasta, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant3 = Restaurant(
   imageUrl: 'assets/images/restaurant3.jpg',
-  name: 'Restaurant 3',
-  address: '200 Main St, New York, NY',
+  name: 'Tropical',
+  address: 'Andheri, Mumbai',
   rating: 2,
   menu: [_burrito, _steak, _burger, _pizza, _salmon],
 );
 final _restaurant4 = Restaurant(
   imageUrl: 'assets/images/restaurant4.jpg',
-  name: 'Restaurant 4',
-  address: '200 Main St, New York, NY',
+  name: 'Hot Valley',
+  address: 'Ghatkopar, Mumbai',
   rating: 3,
   menu: [_burrito, _ramen, _pancakes, _salmon],
 );
@@ -71,31 +71,31 @@ final currentUser = User(
   name: 'Rebecca',
   orders: [
     Order(
-      date: 'Nov 10, 2019',
+      date: 'Nov 10, 2020',
       food: _steak,
       restaurant: _restaurant2,
       quantity: 1,
     ),
     Order(
-      date: 'Nov 8, 2019',
+      date: 'Nov 8, 2020',
       food: _ramen,
       restaurant: _restaurant0,
       quantity: 3,
     ),
     Order(
-      date: 'Nov 5, 2019',
+      date: 'Nov 5, 2020',
       food: _burrito,
       restaurant: _restaurant1,
       quantity: 2,
     ),
     Order(
-      date: 'Nov 2, 2019',
+      date: 'Nov 2, 2020',
       food: _salmon,
       restaurant: _restaurant3,
       quantity: 1,
     ),
     Order(
-      date: 'Nov 1, 2019',
+      date: 'Nov 1, 2020',
       food: _pancakes,
       restaurant: _restaurant4,
       quantity: 1,
@@ -103,31 +103,31 @@ final currentUser = User(
   ],
   cart: [
     Order(
-      date: 'Nov 11, 2019',
+      date: 'Nov 11, 2020',
       food: _burger,
       restaurant: _restaurant2,
       quantity: 2,
     ),
     Order(
-      date: 'Nov 11, 2019',
+      date: 'Nov 11, 2020',
       food: _pasta,
       restaurant: _restaurant2,
       quantity: 1,
     ),
     Order(
-      date: 'Nov 11, 2019',
+      date: 'Nov 11, 2020',
       food: _salmon,
       restaurant: _restaurant3,
       quantity: 1,
     ),
     Order(
-      date: 'Nov 11, 2019',
+      date: 'Nov 11, 2020',
       food: _pancakes,
       restaurant: _restaurant4,
       quantity: 3,
     ),
     Order(
-      date: 'Nov 11, 2019',
+      date: 'Nov 11, 2020',
       food: _burrito,
       restaurant: _restaurant1,
       quantity: 2,
